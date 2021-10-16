31235ad49712126c90b18e0f323c714af4366af8
John Connor <zkmd2012@inbox.ru>
2021-10-16 12:05:18 +0500

    2 Component lifecycle hooks explained

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock">
       click me
   </div>
 </template>
@@ -7,7 +7,25 @@
 <script>
 export default {
     props: ['delay'],
-}
+    data() {
+        return {
+            showBlock: false,
+        }
+    },
+    //lifecycle hooks are written here too
+    mounted() {
+        console.log("mounted")
+        setTimeout(()=> {
+            this.showBlock = true
+        }, this.delay)
+    },
+    updated() {
+        console.log("updated")
+    },
+    unmounted() {
+        console.log("unmounted")
+    }
+} 
 </script>
 
 <style>
