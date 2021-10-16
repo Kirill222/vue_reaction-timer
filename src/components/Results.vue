d1a3f5e46ec268895d6b1335408c2c661379e452
John Connor <zkmd2012@inbox.ru>
2021-10-16 13:37:37 +0500

    5 Rank messages are added with IF statement

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,28 @@
 <template>
-    <h1>Your reaction time {{reactionTime}}</h1>
+    <p>Your reaction time {{reactionTime}}</p>
+    <br>
+    <span>{{rank}}</span>
 </template>
 
 <script>
 export default {
     props: ['reactionTime'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.reactionTime < 250) {
+            this.rank = "Awesome"
+        } else if (this.reactionTime < 400) {
+            this.rank = "Not bad"
+        } else if (this.reactionTime < 600) {
+            this.rank = "Try better"
+        } else {
+            this.rank = "You are slow"
+        }
+    }
 }
 </script>
 
