1906ff8ed5077bafd79e0e423c21a7e6adac9f40
John Connor <zkmd2012@inbox.ru>
2021-10-16 11:37:56 +0500

    1 Start reaction timer project. Components created

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Reaction timer</h1>
+  <button @click="start" :disabled="isPlaying" >Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
+  },
+  components: {
+    Block,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #502c42;
+  margin-top: 60px;
+}
+</style>
